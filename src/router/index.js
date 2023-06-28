4e5fda98f279a2f39749f07d61ecd261efc6d3ef
jiangjc <592791588@qq.com>
2023-06-28 14:16:07 +0800

    vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-export const routes = [
+const routes = [
   'vue实例',
   '模板语法',
   '计算属性与侦听器',
@@ -19,7 +19,23 @@ export const routes = [
     component: () => import('../views/' + filename + '.vue'),
   };
 });
+routes.push({
+  path: '/user/:id',
+  name: '嵌套路由',
+  component: () => import('../views/嵌套路由/index.vue'),
+  children: [
+    // 未匹配到其他子路由时
+    { path: '', component: UserHome },
+    {
+      // 当 /user/:id/profile 匹配成功，
+      // UserProfile 会被渲染在 User 的 <router-view> 中
+      path: 'profile',
+      component: () => import('../views/嵌套路由/UserProfile.vue'),
+    },
+  ],
+});
 
+export { routes };
 const router = new VueRouter({
   routes,
 });
